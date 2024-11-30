cb28487257a04e081adadc50dfab26ff4e83f23b
Ismoilkhan22 <ismoilkhan2205@gmail.com>
2024-11-30 21:29:55 +0500

    cart

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CART_SESSION_ID = 'cart'
+
 
 # Application definition
 
@@ -39,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # apps
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [

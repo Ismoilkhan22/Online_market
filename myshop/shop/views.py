d77eefebea919fd6b32e1ce5251fda71937642b0
Ismoilkhan22 <ismoilkhan2205@gmail.com>
2024-12-02 15:51:33 +0500

    Registering customer orders

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
-
+from cart.forms import CartAddProductForm
 
 def product_list(request, category_slug=None):
     category = None
@@ -21,5 +21,7 @@ def product_detail(request, pk, slug):
                                 id=pk,
                                 slug=slug,
                                 available=True)
+    cart_product_form = CartAddProductForm()
 
-    return render(request, 'shop/product/detail.html', {'product': product})
+    return render(request, 'shop/product/detail.html', {'product': product,
+                                                        'cart_product_form':cart_product_form})
